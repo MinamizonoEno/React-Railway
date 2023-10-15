93c17f148667acedc369badb09ef6799bb7c6395
MinamizonoEno <a02142211@gmail.com>
2023-10-15 22:03:02 +0900

    fix:命名とリストの変更

--- forum-app/src/ThreadList.jsx
+++ forum-app/src/ThreadList.jsx
@@ -2,23 +2,27 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const ThreadList = () => {
-  const [Thead, setThead] = useState([]);
+  const [theads, setTheads] = useState([]);
   useEffect(() => {
     axios
       .get(
-        "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads?offset=1"
+        "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads?offset=0"
       )
       .then((response) => {
-        setThead(response.data);
+        setTheads(response.data);
       });
   }, []);
   const ThreadGet = () => {
-    console.log(Thead);
-    if (Thead !== undefined) {
-      return Thead.map((data) => <div key={data.id}>{data.title}</div>);
+    if (theads !== undefined) {
+      return (
+        <ul>
+          {theads.map((data) => (
+            <il key={data.id}>{data.title}</il>
+          ))}
+        </ul>
+      );
     }
   };
-
   return (
     <div>
       <ThreadGet />
