cd30980c243f0574e99ccde3656113860969ebb6
MinamizonoEno <a02142211@gmail.com>
2023-10-16 00:10:38 +0900

    feat:新規スレッド作成画面完成

--- forum-app/src/ThreadPost.jsx
+++ forum-app/src/ThreadPost.jsx
@@ -1,11 +1,40 @@
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const ThreadPost = () => {
   const navigate = useNavigate();
 
+  //受け取った文をapiに投げる関数
+  const NewThreadPost = (title) => {
+    axios
+      .post(
+        "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads",
+        { title: title }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  //formの投稿ボタンが押されると発火してform内の文を関数に渡す
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { value: threadTitle } = event.target.threadTitle;
+    NewThreadPost(threadTitle);
+  };
+
   return (
     <>
-      <div>init</div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          新規スレッドタイトル
+          <input type="text" name="threadTitle" defaultValue="" />
+        </label>
+        <input type="submit" value="投稿" />
+      </form>
       <button
         onClick={() => {
           navigate("/");
